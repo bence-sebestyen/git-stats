066c2f7733e5e3765e3d1dda932ead37e632f07f
Bence Sebestyén <bence.sebestyen.dev@gmail.com>
2025-12-22 01:11:21 +0100

    feat: implement StatCard in UserDetails

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,4 +1,6 @@
 import type { IUser } from "../types";
+import { MapPin, FolderGit2, User, UserPlus, CalendarPlus } from "lucide-react";
+import StatCard from "./StatCard";
 
 interface Props {
   profileInfo: IUser;
@@ -6,31 +8,46 @@ interface Props {
 
 function UserDetails({ profileInfo }: Props) {
   return (
-    <div>
-      <div>
-        <img src={profileInfo.avatar_url} />
-        <h1>{profileInfo.name}</h1>
-        <h2>@{profileInfo.login}</h2>
-        <p>{profileInfo.bio}</p>
-        <p>{profileInfo.location}</p>
+    <div className="mx-auto p-8 shadow-2xl">
+      <div className="flex flex-col items-center gap-x-20 mb-10 justify-center">
+        <img
+          src={profileInfo.avatar_url}
+          className="w-32 h-32 rounded-full object-cover shadow-lg shadow-purple-500/20"
+          alt="Profile"
+        />
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">{profileInfo.name}</h1>
+          <h2 className="text-purple-400 font-mono text-lg">
+            @{profileInfo.login}
+          </h2>
+          <p className="mt-2 font-medium">{profileInfo.bio}</p>
+          <p className="text-sm flex items-center justify-center gap-1 mt-1">
+            {profileInfo.location && (
+              <>
+                <MapPin size={16} />
+                {profileInfo.location}
+              </>
+            )}
+          </p>
+        </div>
       </div>
-      <div>
-        <div>
-          <span>Repositories</span>
-          <span>{profileInfo.public_repos}</span>
-        </div>
-        <div>
-          <span>Followers</span>
-          <span>{profileInfo.followers}</span>
-        </div>
-        <div>
-          <span>Following</span>
-          <span>{profileInfo.following}</span>
-        </div>
-        <div>
-          <span>Created</span>
-          <span>{profileInfo.created_at}</span>
-        </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <StatCard
+          title="Repositories"
+          value={profileInfo.public_repos}
+          Icon={FolderGit2}
+        />
+        <StatCard title="Followers" value={profileInfo.followers} Icon={User} />
+        <StatCard
+          title="Following"
+          value={profileInfo.following}
+          Icon={UserPlus}
+        />
+        <StatCard
+          title="Created"
+          value={profileInfo.created_at.substring(0, 10)}
+          Icon={CalendarPlus}
+        />
       </div>
     </div>
   );
