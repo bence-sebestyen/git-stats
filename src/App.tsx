6bb06a6f816472b09ff6da61e02a3389e721bc1a
Bence Sebestyén <bence.sebestyen.dev@gmail.com>
2025-12-23 11:24:02 +0100

    feat: add Navbar with navigation to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -6,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/user/:username" element={<ProfilePage />}></Route>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="p-4 backdrop-blur-md sticky top-0 z-50">
+      <div className="max-w-4xl px-4">
+        <Link to="/" className="flex items-center gap-3 w-fit group">
+          <span className="text-xl font-bold text-white hover:scale-105 transition-transform duration-200">
+            Git<span className="text-purple-500">Stats</span>
+          </span>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
