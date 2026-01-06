03daeeaf8145e0f580d0619b49fcceb0182f4ea7
Bence Sebestyén <bence.sebestyen.dev@gmail.com>
2026-01-06 14:44:15 +0100

    chore: add github reference on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,9 +9,26 @@ function HomePage() {
   }
 
   return (
-    <div className="flex h-lvh flex-col items-center justify-center">
-      <h1 className="mb-20 text-5xl">visualize your impact</h1>
-      <SearchForm onSearch={onSearch} />
+    <div className="min-h-[calc(100vh-64px)] flex flex-col items-center justify-center">
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <h1 className="mb-16 text-5xl uppercase font-mono font-bold">
+          visualize your impact
+        </h1>
+        <SearchForm onSearch={onSearch} />
+      </div>
+      <footer className="mt-auto py-10">
+        <a
+          className="flex gap-4 items-center"
+          href="https://www.github.com/bence-sebestyen"
+          target="_blank"
+          rel="noopener norefferer"
+        >
+          <img src="/github-mark.png" className="w-8 h-8" />
+          <p className="font-mono text-slate-700 hover:text-purple-900">
+            bence-sebestyen
+          </p>
+        </a>
+      </footer>
     </div>
   );
 }
