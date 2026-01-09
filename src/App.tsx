9f94e7424eaa8f65e193c4f045c8cd98ace9b505
bence-sebestyen <bence.sebestyen@mailbox.unideb.hu>
2026-01-09 20:15:43 +0100

    fix: correct vercel analytics import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
